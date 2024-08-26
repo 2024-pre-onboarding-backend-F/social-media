3fc8e68128639e8eaf81578e4a5db1b64046ec57
ssunnykku <108388578+ssunnykku@users.noreply.github.com>
2024-08-26 13:39:36 +0900

    refactor: 테스트용 security 설정

--- src/main/java/wanted/media/user/config/SecurityConfig.java
+++ src/main/java/wanted/media/user/config/SecurityConfig.java
@@ -1,7 +1,36 @@
 package wanted.media.user.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig {
+
+    /**
+     * 테스트용 메서드
+     */
+    @Bean
+    @ConditionalOnProperty(name = "spring.h2.console.enabled", havingValue = "true")
+    public WebSecurityCustomizer configureH2ConsoleEnable() {
+        return web -> web.ignoring()
+                .requestMatchers(PathRequest.toH2Console());
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                        request -> request.requestMatchers("/**").permitAll()
+                                .anyRequest().authenticated())
+                .csrf(csrf -> csrf.disable());
+        
+        return http.build();
+    }
 }
