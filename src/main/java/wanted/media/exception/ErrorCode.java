a47f35bc6cdb5ee5064372b6d9c08c145037b0ef
jeongeungyeong <jeongeung1124@gmail.com>
2024-08-26 18:35:41 +0900

    feat : 에러를 처리하기 위한 Exception 생성

--- src/main/java/wanted/media/exception/ErrorCode.java
+++ src/main/java/wanted/media/exception/ErrorCode.java
@@ -1,15 +1,19 @@
 package wanted.media.exception;
 
-import org.springframework.http.HttpStatus;
-
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-	ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 엔티티입니다.");
+    ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 엔티티입니다."),
 
-	private final HttpStatus status;
-	private final String message;
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+    VERIFICATION_CODE_MISMATCH(HttpStatus.BAD_REQUEST, "인증코드가 일치하지 않습니다."),
+    VERIFICATION_CODE_EXPIRED(HttpStatus.BAD_REQUEST, "만료된 인증코드입니다.");
+
+    private final HttpStatus status;
+    private final String message;
 }
--- src/main/java/wanted/media/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/wanted/media/exception/handler/GlobalExceptionHandler.java
@@ -4,15 +4,23 @@ import org.apache.coyote.BadRequestException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
+import wanted.media.exception.BaseException;
+import wanted.media.exception.ErrorCode;
 import wanted.media.exception.ErrorResponse;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-	@ExceptionHandler(BadRequestException.class)
-	public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException e) {
-		return ResponseEntity.badRequest()
-			.body(new ErrorResponse(400, e.getMessage()));
-	}
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException e) {
+        return ResponseEntity.badRequest()
+                .body(new ErrorResponse(400, e.getMessage()));
+    }
+
+    @ExceptionHandler(BaseException.class)
+    public ResponseEntity<ErrorResponse> handleBaseException(BaseException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        return ResponseEntity.status(errorCode.getStatus())
+                .body(new ErrorResponse(errorCode.getStatus().value(), errorCode.getMessage()));
+    }
 }
